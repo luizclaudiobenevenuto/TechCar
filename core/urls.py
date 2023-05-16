f06f99969c52944f7e8af234a78c71b3a7fe2afb
LuizClaudio <luizclaudiobenevenutomota@gmail.com>
2023-05-16 16:06:51 -0300

    Ajustes

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, ios
 
 urlpatterns = [
-    path('', index),
+    path('index.html', index),
+    path('ios.html', ios),
 
 
 ]
